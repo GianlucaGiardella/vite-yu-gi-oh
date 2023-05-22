885b25a5e7ab0d674d9b77c42cbec9bad28c4c35
GianluGiarde <gianlugiarde@gmail.com>
2023-05-22 19:11:01 +0200

    add: store, template & style

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -1,7 +1,13 @@
 <script>
 import Card from "./Card.vue";
+import { store } from "../store";
 
 export default {
+  data() {
+    return {
+      store,
+    };
+  },
   components: {
     Card,
   },
@@ -9,7 +15,23 @@ export default {
 </script>
 
 <template>
-  <Card />
+  <ul>
+    <Card
+      v-for="card in store.cardList"
+      :key="card.id"
+      :cardObj="card"
+      class="flex-item"
+    />
+  </ul>
 </template>
 
-<style></style>
+<style lang="scss" scoped>
+ul {
+  padding: 1rem;
+  display: grid;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
+  column-gap: 1rem;
+  row-gap: 1rem;
+  background-color: white;
+}
+</style>
