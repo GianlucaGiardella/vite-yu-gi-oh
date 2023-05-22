deadf4e1cf686f2024b8cf14ed16be142775fea8
GianluGiarde <gianlugiarde@gmail.com>
2023-05-22 19:10:47 +0200

    add: props, template & style

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,7 +1,47 @@
 <script>
-export default {};
+export default {
+  props: {
+    cardObj: Object,
+  },
+};
 </script>
 
-<template></template>
+<template>
+  <li>
+    <img :src="cardObj.card_images[0].image_url" />
+    <h2>{{ cardObj.name }}</h2>
+    <p>{{ cardObj.archetype }}</p>
+  </li>
+</template>
 
-<style></style>
+<style lang="scss" scoped>
+.flex-item {
+  flex: 0 0 20%;
+}
+
+li {
+  padding-bottom: 0.5rem;
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  align-items: center;
+  background-color: #d48f38;
+  color: white;
+}
+
+li {
+  img {
+    display: block;
+    width: 100%;
+  }
+
+  h2 {
+    font-size: 1rem;
+    text-align: center;
+  }
+
+  p {
+    color: black;
+  }
+}
+</style>
