b28634be25fb209ed5ec3a470aee104b17f9adc8
GianluGiarde <gianlugiarde@gmail.com>
2023-05-23 10:03:16 +0200

    create: results / add: script, template & style

---
+++ src/components/Results.vue
@@ -0,0 +1,26 @@
+<script>
+import { store } from "../store";
+
+export default {
+  data() {
+    return {
+      store,
+    };
+  },
+};
+</script>
+
+<template>
+  <div class="results">
+    <p>Found {{ store.cardList.length }} cards</p>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.results {
+  padding: 1.5rem;
+  font-weight: 600;
+  color: white;
+  background-color: #212529;
+}
+</style>
